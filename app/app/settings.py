807715b30272d9c69c9b90e3ca34ab575af96e17
Orlando Garcia <garciaorlando@gmail.com>
2020-01-19 17:01:43 -0700

    quick update

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'lzrm^6q60cr(44ulmf*ek=)jo^c)9660vo$^g&mjhd-fr^i&ei'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
